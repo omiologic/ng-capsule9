ea39d02a5bdfc0a7220d49561926b35178aae159
Daniel Kang <dkinsk@gmail.com>
2017-07-22 14:09:32 -0400

    starting first spec

---
+++ src/lib/components/column/columns.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed, async } from '@angular/core/testing';
+import { ColumnsComponent } from './columns.component';
+
+describe('ColumnsComponent', () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+		  declarations: [
+		    ColumnsComponent
+		  ]
+		}).compileComponents();
+	}));
+	it('Should create the columns', async(() => {
+		const fixture = TestBed.createComponent(ColumnsComponent);
+		const component = fixture.debugElement.componentInstance;
+
+		expect(component).toBeTruthy();
+	}));
+})
